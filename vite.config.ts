58bc7fc6891e17edf986fe77e1220431a69265c8
gabileaderty dev <contacto@invercamtrix.com>
2026-01-06 17:44:53 -0300

    Fix: Configure Vite to properly copy assets folder to build

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
+  publicDir: 'assets',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    assetsDir: 'assets',
     rollupOptions: {
       input: {
         main: './index.html',
